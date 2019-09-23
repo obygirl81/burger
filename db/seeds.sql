2f3be7b227a803972ead2602c428b87b6424ef34
Obiageli Ogbutor <obygirl81@gmail.com>
2019-09-23 02:02:04 -0600

    updated seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Chesseburger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Barbecue Chesseburger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Triple Chesseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Chesseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Barbecue Chesseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Jalapeno Triple Chesseburger', true);
